53155cf32bc642468bf801a9f1369122bec89792
Amit Kumar <20bcs028@iiitdmj.ac.in>
2021-12-05 14:06:33 +0530

    First commit

---
+++ predict_page.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("Software developer Salary Prediction")
+
+    st.write("""### We need some information to predict the salary""")
+
+    countries = (
+        "United States of America",
+        "India",
+        "Germany",
+        "United Kingdom of Great Britain and Northern Ireland",
+        "Canada",
+        "France",
+        "Brazil",
+        "Spain",
+        "Netherlands",
+        "Australia",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden",
+        "Turkey",
+        "Switzerland",
+        "Israel",
+        "Norway",
+        )
+
+    education = (
+        'Master’s degree',
+        'Bachelor’s degree', 
+        'Post grad',
+        'Less than a Bachelors',
+        )
+
+    country = st.selectbox("Country",countries)
+    education = st.selectbox("Education level",education)
+
+    experience = st.slider("Years of experience", 0, 50, 3)
+
+    ok = st.button("Calculate Salary")
+
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:, 0])
+        X[:, 1] = le_education.transform(X[:, 1])
+        X = X.astype(float)
+
+        salary = regressor.predict(X)
+        st.subheader(f"The estimated salary is ${salary[0]:.2f}")
